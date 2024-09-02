5ad1dc7b4e28890211e9bd3510a98bb94c4c4d1a
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-09-02 16:54:39 -0300

    requisição POST

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 import ui from "./ui.js"
+import api from "./api.js"
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.renderizarPensamentos()
+
+    const formularioPensamento = document.getElementById("pensamento-form")
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
 })
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+    const id = document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
+
+    try {
+        await api.salvarPensamento({ conteudo, autoria })
+        ui.renderizarPensamentos()
+    } 
+    catch {
+        alert("Erro ao salvar pensamento")
+    }
+
+}
--- js/ui.js
+++ js/ui.js
@@ -2,11 +2,12 @@ import api from "./api.js"
 
 const ui = {
     async renderizarPensamentos() {
-        const listaPensamentos = document.getElementById('lista-pensamentos')
+        const listaPensamentos = document.getElementById("lista-pensamentos")
 
         try {
             const pensamentos = await api.buscarPensamentos()
-            pensamentos.forEach(pensamento => {
+            pensamentos.forEach(ui.adicionarPensamentoNaLista) /* (pensamento => {
+                
                 listaPensamentos.innerHTML += `
                     <li class="li-pensamento" data-id="${pensamento.id}">
                     <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
@@ -14,10 +15,37 @@ const ui = {
                     <div class="pensamento-autoria">${pensamento.autoria}</div>
                     </li>
                 `
-            })
+                
+            }) */
         } catch {
-            alert('Erro ao buscar pensamentos')
+            alert("Erro ao buscar pensamentos")
         }
+    },
+
+    adicionarPensamentoNaLista(pensamento) {
+        const listaPensamentos = document.getElementById("lista-pensamentos")
+        const li = document.createElement("li")
+        li.setAttribute("data-id", pensamento.id)
+        li.classList.add("li-pensamento")
+
+        const iconeAspas = document.createElement("img")
+        iconeAspas.src = "assets/imagens/aspas-azuis.png"
+        iconeAspas.alt = "Aspas azuis"
+        iconeAspas.classList.add("icone-aspas")
+
+        const pensamentoConteudo = document.createElement("div")
+        pensamentoConteudo.textContent = pensamento.conteudo
+        pensamentoConteudo.classList.add("pensamento-conteudo")
+
+        const pensamentoAutoria = document.createElement("div")
+        pensamentoAutoria.textContent = pensamento.autoria
+        pensamentoAutoria.classList.add("pensamento-autoria")
+
+        li.appendChild(iconeAspas)
+        li.appendChild(pensamentoConteudo)
+        li.appendChild(pensamentoAutoria)
+        listaPensamentos.appendChild(li)
+
     }
 }
 
