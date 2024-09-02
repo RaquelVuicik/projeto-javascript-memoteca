9a1456aafa653c220f08e841797fba883d0fb73d
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-09-02 18:04:02 -0300

    botao cancelar

--- js/main.js
+++ js/main.js
@@ -5,8 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
     ui.renderizarPensamentos()
 
     const formularioPensamento = document.getElementById("pensamento-form")
+    const botaoCancelar = document.getElementById("botao-cancelar")
+
     formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+    botaoCancelar.addEventListener("click", manipularCancelamento)
 })
+
+function manipularCancelamento() {
+    ui.limparFormulario();
+}
 
 async function manipularSubmissaoFormulario(event) {
     event.preventDefault();
@@ -17,9 +24,11 @@ async function manipularSubmissaoFormulario(event) {
     try {
         await api.salvarPensamento({ conteudo, autoria })
         ui.renderizarPensamentos()
+
     } 
     catch {
         alert("Erro ao salvar pensamento")
     }
 
 }
+
--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,11 @@
 import api from "./api.js"
 
 const ui = {
+
+    limparFormulario() {
+        document.getElementById("pensamento-form").reset()
+    },
+    
     async renderizarPensamentos() {
         const listaPensamentos = document.getElementById("lista-pensamentos")
 
