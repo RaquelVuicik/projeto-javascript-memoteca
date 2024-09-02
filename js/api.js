04a025d6894492f17eddfed9f67039c17b30076b
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-09-02 18:31:30 -0300

    buscar e editar pensamento

--- js/api.js
+++ js/api.js
@@ -20,7 +20,33 @@ const api = {
             })
             return await response.json()
         } catch {
-            alert('Erro ao buscar pensamentos!')
+            alert('Erro ao salvar pensamento')
+            throw error
+        }
+    },
+
+    async buscarPensamentoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+            return await response.json()
+        } catch {
+            alert('Erro ao buscar pensamento')
+            throw error
+        }
+    },
+
+    async editarPensamento(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        } catch {
+            alert('Erro ao aditar pensamento')
             throw error
         }
     }
