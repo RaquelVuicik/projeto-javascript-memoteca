23282a5e6967d7796625ff0da31b59c6cef0f8c1
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-07 14:48:44 -0300

    add filtragem de pensamentos

--- js/api.js
+++ js/api.js
@@ -53,6 +53,21 @@ const api = {
             alert('Erro ao excluir um pensamento')
             throw error
         }
+    },
+
+    async buscarPensamentosPorTermo(termo) {
+        try {
+            const pensamentos = await this.buscarPensamentos()
+            const termoEmMinusculas = termo.toLowerCase()
+
+            const pensamentosFiltrados = pensamentos.filter(pensamento => {
+            return (pensamento.conteudo.toLowerCase().includes(termoEmMinusculas)) || pensamento.autoria.toLowerCase().includes(termoEmMinusculas)
+            })
+            return pensamentosFiltrados   
+        } catch (error) {
+            alert("Erro ao filtrar pensamentos")
+            throw error
+        }
     }
 }
 
