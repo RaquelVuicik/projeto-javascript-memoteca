d4efd81b844809c986361438e7e1af99056ed51c
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-08 11:06:09 -0300

    criando e validando inputs de data

--- js/main.js
+++ js/main.js
@@ -18,12 +18,17 @@ async function manipularSubmissaoFormulario(event) {
     const id = document.getElementById("pensamento-id").value
     const conteudo = document.getElementById("pensamento-conteudo").value
     const autoria = document.getElementById("pensamento-autoria").value
+    const data = document.getElementById("pensamento-data").value
+
+    if(!validarData(data)) {
+        alert("Não é permitido o cadastro de datas futuras. Selecione outra data.")
+    }
 
     try {
         if(id) {
-            await api.editarPensamento({ id, conteudo, autoria })
+            await api.editarPensamento({ id, conteudo, autoria, data })
         } else {
-            await api.salvarPensamento({ conteudo, autoria })
+            await api.salvarPensamento({ conteudo, autoria, data })
         }
         ui.renderizarPensamentos()
     } 
@@ -45,4 +50,10 @@ async function manipularBusca() {
     } catch (error) {
         alert("Erro ao realizar busca")
     }
+}
+
+function validarData(data) {
+    const dataAtual = new Date()
+    const dataInserida = new Date(data)
+    return dataInserida <= dataAtual
 }
