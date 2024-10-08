a5a59b5591599ca0b989d9623f15c2d8531f589c
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-08 09:40:39 -0300

    adicionando comportamento botao favorito

--- js/api.js
+++ js/api.js
@@ -68,6 +68,16 @@ const api = {
             alert("Erro ao filtrar pensamentos")
             throw error
         }
+    },
+
+    async atualizarFavorito(id, favorito) {
+        try {
+            const response = await axios.patch(`${URL_BASE}/pensamentos/${id}`, { favorito })
+            return response.data
+        } catch (error) {
+            alert("Erro ao atualizar favorito")
+            throw error
+        }
     }
 }
 
--- js/ui.js
+++ js/ui.js
@@ -84,9 +84,17 @@ const ui = {
 
         const botaoFavorito = document.createElement("button")
         botaoFavorito.classList.add("botao-favorito")
+        botaoFavorito.onclick = async () => {
+            try {
+                await api.atualizarFavorito(pensamento.id, !pensamento.favorito)
+                ui.renderizarPensamentos()
+            } catch (error) {
+                alert("Erro ao atualizar pensamento")
+            }
+        }
 
         const iconeFavorito = document.createElement("img")
-        iconeFavorito.src = "assets/imagens/icone-favorito_outline.png"
+        iconeFavorito.src = pensamento.favorito ? "assets/imagens/icone-favorito.png" : "assets/imagens/icone-favorito_outline.png"
         iconeFavorito.alt = "Ícone de favorito"
         botaoFavorito.appendChild(iconeFavorito)
 
